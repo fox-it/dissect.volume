c4acbc510a7f0e5b3f7784781de2f057c7c6f919
Schamper <1254028+Schamper@users.noreply.github.com>
2022-11-11 15:49:19 +0100

    Detect BSD in Disk class

--- tests/test_bsd.py
+++ tests/test_bsd.py
@@ -27,27 +27,27 @@ def test_bsd(bsd):
 
 
 def test_bsd64(bsd64):
-    # The disklabel64 partition is a separate GPT partition in this test data
     d = disk.Disk(bsd64)
-    vol = d.partitions[1].open()
-    vs = BSD(vol)
 
-    assert len(vs.partitions) == 3
+    assert isinstance(d.scheme, BSD)
+    assert len(d.partitions) == 4
 
-    assert vs.partitions[0].number == 1
-    assert vs.partitions[0].offset == 0x100000
-    assert vs.partitions[0].size == 0x1000
-    assert vs.partitions[0].type == 7
-    assert vs.partitions[0].open().read(512) == b"\x01" * 512
+    # Index 0 is another GPT partition
 
-    assert vs.partitions[1].number == 2
-    assert vs.partitions[1].offset == 0x101000
-    assert vs.partitions[1].size == 0x1000
-    assert vs.partitions[1].type == 7
-    assert vs.partitions[1].open().read(512) == b"\x02" * 512
+    assert d.partitions[1].number == 1
+    assert d.partitions[1].offset == 0x100000
+    assert d.partitions[1].size == 0x1000
+    assert d.partitions[1].type == 7
+    assert d.partitions[1].open().read(512) == b"\x01" * 512
 
-    assert vs.partitions[2].number == 5
-    assert vs.partitions[2].offset == 0x102000
-    assert vs.partitions[2].size == 0x1000
-    assert vs.partitions[2].type == 7
-    assert vs.partitions[2].open().read(512) == b"\x05" * 512
+    assert d.partitions[2].number == 2
+    assert d.partitions[2].offset == 0x101000
+    assert d.partitions[2].size == 0x1000
+    assert d.partitions[2].type == 7
+    assert d.partitions[2].open().read(512) == b"\x02" * 512
+
+    assert d.partitions[3].number == 5
+    assert d.partitions[3].offset == 0x102000
+    assert d.partitions[3].size == 0x1000
+    assert d.partitions[3].type == 7
+    assert d.partitions[3].open().read(512) == b"\x05" * 512
