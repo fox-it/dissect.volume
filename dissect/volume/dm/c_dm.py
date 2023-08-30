dbeccf1b9166439c56c748264f1f292fbf57a4f8
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2023-08-30 12:58:07 +0200

    Add initial support for dm-thin (#19)

---
+++ dissect/volume/dm/c_dm.py
@@ -0,0 +1,93 @@
+from dissect.cstruct import cstruct
+
+dm_def = """
+/* DM BTree */
+enum node_flags {
+    INTERNAL_NODE = 1,
+    LEAF_NODE = 1 << 1
+};
+
+/*
+ * Every btree node begins with this structure.  Make sure it's a multiple
+ * of 8-bytes in size, otherwise the 64bit keys will be mis-aligned.
+ */
+struct node_header {
+    uint32  csum;
+    uint32  flags;
+    uint64  blocknr;                /* Block this node is supposed to live in. */
+
+    uint32  nr_entries;
+    uint32  max_entries;
+    uint32  value_size;
+    uint32  padding;
+};
+
+struct btree_node {
+    struct node_header header;
+    // uint64 keys[];
+};
+
+/* Thin */
+#define THIN_SUPERBLOCK_MAGIC 27022010
+#define THIN_SUPERBLOCK_LOCATION 0
+#define THIN_VERSION 2
+#define SECTOR_TO_BLOCK_SHIFT 3
+
+/* This should be plenty */
+#define SPACE_MAP_ROOT_SIZE 128
+
+/*
+ * Little endian on-disk superblock and device details.
+ */
+struct thin_disk_superblock {
+    uint32  csum;                   /* Checksum of superblock except for this field. */
+    uint32  flags;
+    uint64  blocknr;                /* This block number, dm_block_t. */
+
+    uint8   uuid[16];
+    uint64  magic;
+    uint32  version;
+    uint32  time;
+
+    uint64  trans_id;
+
+    /*
+     * Root held by userspace transactions.
+     */
+    uint64  held_root;
+
+    uint8   data_space_map_root[SPACE_MAP_ROOT_SIZE];
+    uint8   metadata_space_map_root[SPACE_MAP_ROOT_SIZE];
+
+    /*
+     * 2-level btree mapping (dev_id, (dev block, time)) -> data block
+     */
+    uint64  data_mapping_root;
+
+    /*
+     * Device detail root mapping dev_id -> device_details
+     */
+    uint64  device_details_root;
+
+    uint32  data_block_size;        /* In 512-byte sectors. */
+
+    uint32  metadata_block_size;    /* In 512-byte sectors. */
+    uint64  metadata_nr_blocks;
+
+    uint32  compat_flags;
+    uint32  compat_ro_flags;
+    uint32  incompat_flags;
+};
+
+struct disk_device_details {
+    uint64 mapped_blocks;
+    uint64 transaction_id;          /* When created. */
+    uint32 creation_time;
+    uint32 snapshotted_time;
+};
+"""
+
+c_dm = cstruct()
+c_dm.load(dm_def)
+
+SECTOR_SIZE = 512
---
+++ dissect/volume/dm/thin.py
@@ -0,0 +1,104 @@
+from __future__ import annotations
+
+from typing import BinaryIO, Optional
+
+from dissect.util.stream import AlignedStream
+
+from dissect.volume.dm.btree import BTree
+from dissect.volume.dm.c_dm import SECTOR_SIZE, c_dm
+from dissect.volume.exceptions import DMError
+
+
+class ThinPool:
+    """Interact with a device-mapper thin-provision pool."""
+
+    def __init__(self, metadata_fh: BinaryIO, data_fh: BinaryIO):
+        self.metadata_fh = metadata_fh
+        self.data_fh = data_fh
+
+        self.metadata = Metadata(metadata_fh)
+
+    def open(self, device_id: int, size_hint: Optional[int] = None) -> ThinDevice:
+        """Open a thin device on this pool.
+
+        No size information is stored in the pool, so it's recommended to provide a size hint.
+
+        Args:
+            device_id: The device ID to open.
+            size_hint: Optional size hint for the device to open.
+        """
+        return ThinDevice(self, device_id, size_hint)
+
+
+class Metadata:
+    """Interact with a device-mapper thin-provision metadata device."""
+
+    def __init__(self, fh: BinaryIO):
+        self.fh = fh
+        self.sb = c_dm.thin_disk_superblock(fh)
+        if self.sb.magic != c_dm.THIN_SUPERBLOCK_MAGIC:
+            raise DMError("Invalid magic for thin-pool metadata superblock")
+
+        self.data_block_size = self.sb.data_block_size
+        self.metadata_block_size = self.sb.metadata_block_size
+
+        self.data_mapping = BTree(self.fh, self.sb.data_mapping_root, self.metadata_block_size)
+        self.device_details = BTree(self.fh, self.sb.device_details_root, self.metadata_block_size)
+
+
+class ThinDevice(AlignedStream):
+    """Implements a readable thin device/volume.
+
+    Args:
+        pool: The pool the device belongs to.
+        device_id: The device ID of the device.
+        size_hint: Optional device size hint.
+    """
+
+    def __init__(self, pool: ThinPool, device_id: int, size_hint: Optional[int] = None):
+        self.pool = pool
+        self.device_id = device_id
+        self.block_size = self.pool.metadata.data_block_size * SECTOR_SIZE
+
+        details_buf = self.pool.metadata.device_details.lookup(device_id)
+        if not details_buf:
+            raise DMError(f"Device ID is not known in pool: {device_id}")
+        self.details = c_dm.disk_device_details(details_buf)
+
+        super().__init__(size_hint, self.block_size)
+
+    def _read(self, offset: int, length: int) -> bytes:
+        data_fh = self.pool.data_fh
+        data_mapping = self.pool.metadata.data_mapping
+
+        block = offset // self.block_size
+
+        result = []
+        while length > 0:
+            block_info = data_mapping.lookup([self.device_id, block])
+            if block_info is None:
+                break
+
+            block_time = int.from_bytes(block_info, "little")
+            data_block, _ = _unpack_block_time(block_time)
+
+            read_size = min(length, self.block_size)
+            data_fh.seek(data_block * self.block_size)
+            result.append(data_fh.read(read_size))
+
+            length -= read_size
+            offset += read_size
+            block += 1
+
+        return b"".join(result)
+
+
+def _unpack_block_time(block_time: int) -> tuple[int, int]:
+    """Unpack a block number and timestamp from a combined block time value.
+
+    Block numbers in device-mapper thin-provisioning store the block number in the upper 40 bits and
+    a timestamp in the lower 24 bits.
+    """
+    block = block_time >> 24
+    time = block_time & ((1 << 24) - 1)
+    return block, time
